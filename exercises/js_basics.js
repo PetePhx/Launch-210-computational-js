191933e20c9c3e8e9eb1438bfd104f7b94820a17
PetePhx <pete0phx@yandex.com>
2018-06-20 16:43:05 -0700

    js_basics #7 - #10

--- exercises/js_basics.js
+++ exercises/js_basics.js
@@ -200,3 +200,92 @@ var userStr = prompt('Please enter a phrase: ');
 console.log('There are ' + userStr.replace(/[^a-z]/ig, '').length + ' alphabetic characters in "' + userStr + '".');
 
 // There are 11 alphabetic characters in "Walk, don't run".
+
+
+// 7. Convert a String to a Number
+
+// For this exercise we're going to learn more about type conversion by implementing our own parseInt function that converts a string of numeric characters (including an optional plus or minus sign) to a number.
+
+// The function takes a string of digits as an argument, and returns the appropriate number. Do not use any of the built-in functions for converting a string to a number type.
+
+// For now, do not worry about leading + or - signs, nor should you worry about invalid characters; assume all characters will be numeric.
+
+function stringToInteger(str) {
+  var DIGITS = {
+    '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9
+  };
+
+  var result = 0;
+  for (var i = 0; i < str.length; i++) { result = 10 * result + DIGITS[str[i]]; }
+  return result;
+}
+
+stringToInteger('4321');      // 4321
+stringToInteger('570');       // 570
+
+
+// 8. Convert a String to a Signed Number
+
+// The previous exercise mimics the parseInt function to a lesser extent. In this exercise, you're going to extend that function to work with signed numbers.
+
+// Write a function that takes a string of digits, and returns the appropriate number as an integer. The string may have a leading + or - sign; if the first character is a +, your function should return a positive number; if it is a -, your function should return a negative number. If there is no sign, return a positive number.
+
+// You may assume the string will always contain a valid number.
+
+function stringToSignedInteger(str) {
+  if (str[0] === '+') {
+    return stringToInteger(str.slice(1));
+  } else if (str[0] === '-') {
+    return -1 * stringToInteger(str.slice(1));
+  } else {
+    return stringToInteger(str);
+  }
+}
+
+stringToSignedInteger('4321');      // 4321
+stringToSignedInteger('-570');      // -570
+stringToSignedInteger('+100');      // 100
+
+
+// 9. Convert a Number to a String
+
+// In the previous two exercises, you developed functions that convert simple numeric strings to signed integers. In this exercise and the next, you're going to reverse those functions.
+
+// You will learn more about converting strings to numbers by writing a function that takes a positive integer or zero, and converts it to a string representation.
+
+// You may not use any of the standard conversion functions available in JavaScript, such as String(), Number.prototype.toString, or an expression such as '' + number. Your function should do this the old-fashioned way and construct the string by analyzing and manipulating the number.
+
+function integerToString(num) {
+  var NUMERALS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var result = '';
+
+  do {
+    result += NUMERALS[num % 10];
+    num = Math.floor(num / 10);
+  } while (num > 0);
+
+  return result.split('').reverse().join('');
+}
+
+integerToString(4321);      // "4321"
+integerToString(0);         // "0"
+integerToString(5000);      // "5000"
+
+
+// 10. Convert a Signed Number to a String
+
+// In the previous exercise, you reimplemented a function that converts non-negative numbers to strings. In this exercise, you're going to extend that function by adding the ability to represent negative numbers.
+
+// You may not use any of the standard conversion functions available in JavaScript, such as String(), Number.prototype.toString, or an expression such as '' + number. You may, however, use the integerToString function from the previous exercise.
+
+function signedIntegerToString(num) {
+  switch (Math.sign(num)) {
+    case 1: return '+' + integerToString(num);
+    case 0: return '0';
+    case -1: return '-' + integerToString(-1 * num);
+  }
+}
+
+signedIntegerToString(4321);      // "+4321"
+signedIntegerToString(-123);      // "-123"
+signedIntegerToString(0);         // "0"

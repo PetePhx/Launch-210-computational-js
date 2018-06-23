57ed93156be1bfcb45aad6619f52901df2e3c8f2
PetePhx <pete0phx@yandex.com>
2018-06-23 15:48:15 -0700

    easy_5 #9, #10

---
+++ small_problems/easy_5/10_reverse_it_p2.js
@@ -0,0 +1,24 @@
+/*
+
+Reverse It Part 2
+
+Write a function that takes a string argument containing one or more words, and returns a new string containing the words from the string argument. All five-or-more letter words should have their letters in reverse order. The string argument will consist of only letters and spaces. Words will be separated by a single space.
+
+Examples:
+
+reverseWords('Professional');             // "lanoisseforP"
+reverseWords('Walk around the block');    // "Walk dnuora the kcolb"
+reverseWords('Launch School');            // "hcnuaL loohcS"
+
+*/
+
+function reverseWords(str) {
+  return str.split(' ')
+            .map(wrd => (wrd.length >= 5 ? wrd.split('').reverse().join(''): wrd))
+            .join(' ');
+}
+
+reverseWords('Professional');             // "lanoisseforP"
+reverseWords('Walk around the block');    // "Walk dnuora the kcolb"
+reverseWords('Launch School');            // "hcnuaL loohcS"
+reverseWords('');                         // ""

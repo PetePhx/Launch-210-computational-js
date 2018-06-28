2b780689a3c6306ff26ef2952c08b7d763fdf1fa
PetePhx <pete0phx@yandex.com>
2018-06-28 16:11:30 -0700

    processReleases.js

---
+++ 7_list_processing/processReleases.js
@@ -0,0 +1,65 @@
+/*
+
+Practice Problem: Processing Releases
+
+Write a Function named processReleaseData that processes the following movie release data:
+
+The Function should generate an Array of Objects that contain only the id and title key/value pairs. You may assume that id, when existing, is an integer greater than 0. Here are the rules:
+
+    Keep only releases that have both id and title data present.
+    Keep only the id and title data for each release.
+
+function processReleaseData(data) {
+  // ...
+}
+
+processReleaseData(newReleases);
+
+// should return:
+[{ id: 70111470, title: 'Die Hard'}, { id: 675465, title: 'Fracture' }];
+
+*/
+
+function processReleaseData(data) {
+  return data.filter(release => release.id && release.title)
+             .map(release => ({ id: release.id, title: release.title }));
+}
+
+// () for arrow function returning object literal
+
+var newReleases = [
+  {
+    'id': 70111470,
+    'title': 'Die Hard',
+    'boxart': 'http://cdn-0.nflximg.com/images/2891/DieHard.jpg',
+    'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+    'rating': [4.0],
+    'bookmark': [],
+  },
+  {
+    'id': 654356453,
+    'boxart': 'http://cdn-0.nflximg.com/images/2891/BadBoys.jpg',
+    'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+    'rating': [5.0],
+    'bookmark': [{ id:432534, time:65876586 }],
+  },
+  {
+    'title': 'The Chamber',
+    'boxart': 'http://cdn-0.nflximg.com/images/2891/TheChamber.jpg',
+    'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+    'rating': [4.0],
+    'bookmark': [],
+  },
+  {
+    'id': 675465,
+    'title': 'Fracture',
+    'boxart': 'http://cdn-0.nflximg.com/images/2891/Fracture.jpg',
+    'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+    'rating': [5.0],
+    'bookmark': [{ id:432534, time:65876586 }],
+  },
+];
+
+processReleaseData(newReleases);
+
+// [{ id: 70111470, title: 'Die Hard'}, { id: 675465, title: 'Fracture' }];

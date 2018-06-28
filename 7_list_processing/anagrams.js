5a1496180792d578697f58ca3fd01cfd80332aa8
PetePhx <pete0phx@yandex.com>
2018-06-28 16:30:06 -0700

    anagrams.js

---
+++ 7_list_processing/anagrams.js
@@ -0,0 +1,16 @@
+/*
+
+Practice Problem: Anagrams
+
+Write a Function named anagram that takes two arguments: a word and an array of words. Your function should return an array that contains all the words from the array argument that are anagrams of the word argument. For example, given the word "listen" and an array of candidate words like "enlist", "google", "inlets", and "banana", the program should return an array that contains "enlist" and "inlets".
+Examples
+
+*/
+
+function anagram(word, list) {
+  var wordSort = word.split('').sort().join('');
+  return list.filter(elm => elm.split('').sort().join('') === wordSort);
+}
+
+anagram('listen', ['enlists', 'google', 'inlets', 'banana']);  // [ "inlets" ]
+anagram('listen', ['enlist', 'google', 'inlets', 'banana']);   // [ "enlist", "inlets" ]

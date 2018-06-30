5ac2596f8b341cc0735d71d4ae64153c988b3d88
PetePhx <pete0phx@yandex.com>
2018-06-30 11:44:01 -0700

    07_palindromic_substrings.js

---
+++ small_problems/list_processing/07_palindromic_substrings.js
@@ -0,0 +1,56 @@
+/*
+
+Palindromic Substrings
+
+Write a function that returns a list of all substrings of a string that are palindromic. That is, each substring must consist of the same sequence of characters forwards as backwards. The substrings in the returned list should be sorted by their order of appearance in the input string. Duplicate substrings should be included multiple times.
+
+You may (and should) use the substrings function you wrote in the previous exercise.
+
+For the purpose of this exercise, you should consider all characters and pay attention to case; that is, 'AbcbA' is a palindrome, but 'Abcba' and 'Abc-bA' are not. In addition, assume that single characters are not palindromes.
+
+Examples:
+
+palindromes('abcd');       // []
+palindromes('madam');      // [ "madam", "ada" ]
+
+palindromes('hello-madam-did-madam-goodbye');
+// returns
+[ "ll", "-madam-", "-madam-did-madam-", "madam", "madam-did-madam", "ada",
+  "adam-did-mada", "dam-did-mad", "am-did-ma", "m-did-m", "-did-", "did",
+  "-madam-", "madam", "ada", "oo" ]
+
+palindromes('knitting cassettes');
+// returns
+[ "nittin", "itti", "tt", "ss", "settes", "ette", "tt" ]
+
+*/
+
+function palindromes(str) {
+  return substrings(str).filter(txt => txt.length > 1 && txt === txt.split('').reverse().join(''));
+}
+
+function substrings(str) {
+  return flat(Array(str.length).fill().map((_, idx) => substringsAtStart(str.slice(idx))));
+}
+
+function substringsAtStart(str) {
+  return Array(str.length).fill().map((_, idx) => str.slice(0, idx + 1));
+}
+
+function flat(arr) {
+  return arr.reduce((acc, elm) =>
+    { Array.isArray(elm) ? acc.push(...elm) : acc.push(elm); return acc; }, []);
+}
+
+console.log(palindromes('abcd'));       // []
+console.log(palindromes('madam'));      // [ "madam", "ada" ]
+
+console.log(palindromes('hello-madam-did-madam-goodbye'));
+// returns
+[ "ll", "-madam-", "-madam-did-madam-", "madam", "madam-did-madam", "ada",
+  "adam-did-mada", "dam-did-mad", "am-did-ma", "m-did-m", "-did-", "did",
+  "-madam-", "madam", "ada", "oo" ]
+
+console.log(palindromes('knitting cassettes'));
+// returns
+[ "nittin", "itti", "tt", "ss", "settes", "ette", "tt" ]

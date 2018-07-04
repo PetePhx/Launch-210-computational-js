89a688a00cb9d3a254895573349949903dfcb39c
PetePhx <pete0phx@yandex.com>
2018-07-03 17:21:51 -0700

    02_delete_vowels.js

---
+++ small_problems/string_text_processing/02_delete_vowels.js
@@ -0,0 +1,15 @@
+/*
+
+Delete Vowels
+
+Write a function that takes an array of strings, and returns an array of the same strings values without the vowels (a, e, i, o, u).
+
+*/
+
+function removeVowels(arr) {
+  return arr.map(str => str.replace(/[aeiou]/ig, ''));
+}
+
+console.log(removeVowels(['abcdefghijklmnopqrstuvwxyz']));         // ["bcdfghjklmnpqrstvwxyz"]
+console.log(removeVowels(['green', 'YELLOW', 'black', 'white']));  // ["grn", "YLLW", "blck", "wht"]
+console.log(removeVowels(['ABC', 'AEIOU', 'XYZ']));                // ["BC", "", "XYZ"]
